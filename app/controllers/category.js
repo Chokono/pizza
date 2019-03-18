6befefec9e157046ef1db13f2e5874a42a7b1a33
chokono <ne_zabivai@tut.by>
2019-03-18 08:23:03 +0300

    some styles

--- app/controllers/category.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-
-export default Controller.extend({
-  actions: {
-    filterByParam(param) {
-      return this.store.query('category', param);
-    }
-  }
-});
